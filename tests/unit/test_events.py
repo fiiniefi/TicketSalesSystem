08b86973bd7b83a71586a9ed5f87cb2bedf43acf
fiiniefi <marcin.drewniak1@vp.pl>
2019-07-06 15:58:04 +0200

    Fix indentation convention

--- tests/unit/test_events.py
+++ tests/unit/test_events.py
@@ -1,12 +1,20 @@
+from mock import Mock
+import pytest
 # noinspection PyUnresolvedReferences
-from tests.unit.connections import db_connection
+from tests.unit.connections import db_connection_base
 from sales_system.events import EventManager
 import sales_system.events
-from mock import Mock
 
 
-def test_GetEvents_PassedCorrectly(db_connection, monkeypatch):
+@pytest.fixture(scope='module')
+def event_manager():
+    return EventManager(Mock())
+
+
+def test_GetEvents_PassedCorrectly(db_connection_base, event_manager, monkeypatch):
     monkeypatch.setattr(sales_system.events, 'datetime', Mock())
-    EventManager(Mock()).get_events(db_connection)
-    db_connection.cursor.execute.assert_called_once()
-    db_connection.cursor.fetchall.assert_called_once_with()
+
+    event_manager.get_events(db_connection_base)
+
+    db_connection_base.cursor.execute.assert_called_once()
+    db_connection_base.cursor.fetchall.assert_called_once_with()
--- tests/unit/test_interface.py
+++ tests/unit/test_interface.py
@@ -2,14 +2,14 @@ import pytest
 from mock import Mock, patch, ANY
 from sales_system import TicketSystem
 # noinspection PyUnresolvedReferences
-from tests.unit.connections import db_connection
+from tests.unit.connections import db_connection_base
 import sales_system.interface
 
 
 @pytest.fixture(scope='module')
-def ticket_system(db_connection):
+def ticket_system(db_connection_base):
     with patch('sales_system.interface.open'):
-        yield TicketSystem(db_connection, Mock())
+        yield TicketSystem(db_connection_base, Mock())
 
 
 @pytest.mark.parametrize('module, function_name, additional_argc, keyword_argv',
@@ -20,9 +20,11 @@ def ticket_system(db_connection):
                           (sales_system.interface.TicketManager, 'get_reservation_info', 1, []),
                           (sales_system.interface.TicketManager, 'get_reservation_statistics', 0, []),
                           ])
-def test_InterfaceKeywords_PassedCorrectly(db_connection, ticket_system, monkeypatch,
+def test_InterfaceKeywords_PassedCorrectly(db_connection_base, ticket_system, monkeypatch,
                                            module, function_name, additional_argc, keyword_argv):
     function_mock = Mock()
     monkeypatch.setattr(module, function_name, function_mock)
+
     getattr(ticket_system, function_name)(*keyword_argv)
-    function_mock.assert_called_once_with(db_connection, *([ANY]*additional_argc))
+
+    function_mock.assert_called_once_with(db_connection_base, *([ANY] * additional_argc))
