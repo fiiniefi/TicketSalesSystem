ae1573754674722f65b5802b3ed828d31bb5efdd
fiiniefi <marcin.drewniak1@vp.pl>
2019-07-06 15:56:36 +0200

    Add new type of connection

--- tests/unit/connections.py
+++ tests/unit/connections.py
@@ -3,5 +3,11 @@ from mock import Mock
 
 
 @pytest.fixture(scope='session')
-def db_connection():
+def db_connection_base():
     return Mock(connection=Mock(), cursor=Mock())
+
+
+@pytest.fixture(scope='session')
+def db_connection_commit(db_connection_base):
+    db_connection_base.commit = Mock()
+    return db_connection_base
