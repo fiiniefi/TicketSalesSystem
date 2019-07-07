d592c8a6af61cc0c52b67a64f585c9f9ea373a41
fiiniefi <marcin.drewniak1@vp.pl>
2019-07-07 04:48:38 +0200

    Fix SQL dump
    From now reload does not cause an error

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 
 setup(name='TicketSalesSystem',
-      version='0.1',
+      version='0.1.1',
       description='Python Distribution Utilities',
       author='Marcin Drewniak',
       author_email='marcin.drewniak1@vp.pl',
